34991d2c06a2483159ba6b9c3b6edbf0e5fb1e07
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-04-24 09:23:26 +0200

    Update poppy_install.sh

--- src/poppy_install.sh
+++ src/poppy_install.sh
@@ -127,21 +127,7 @@ install_poppy_software() {
 
   for repo in pypot poppy-creature $creatures
   do
-    cd $POPPY_ROOT
-
-    if [ ! -z "$use_stable_release" ]; then
-      pip install $repo
-    else
-      git clone https://github.com/poppy-project/$repo.git
-
-      cd $repo
-      if [[ $repo == poppy-* ]]; then
-        cd software
-      fi
-
-      python setup.py develop
-
-    fi
+    pip install $repo
   done
 }
 
